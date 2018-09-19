1315cde56b41769ebc7ff12eeb529007637aa7b2
Amelie Chan <amelie.chan@gmail.com>
2018-09-19 16:51:36 +0100

    Remove unused Button component

--- stories/index.js
+++ stories/index.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
 import DrawingTool from '../DrawingTool'
 import OpacitySlider from '../components/OpacitySlider/OpacitySlider'
 import WidthSlider from '../components/WidthSlider/WidthSlider'
-import Button from '../components/Button/Button'
 import ColorButton from '../components/ColorButton/ColorButton'
 
   storiesOf("DrawingTool", module)
